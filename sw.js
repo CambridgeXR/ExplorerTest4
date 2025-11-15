177abf8d1dc2b2d047e3cfef19093e8367f6daef
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-11-15 19:34:07 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,43 @@
-const CACHE_NAME = 'vr-explorer-v1';
-const OFFLINE_URLS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+const CACHE_NAME = "vr-explorer-v2";
+const ASSETS_TO_CACHE = [
+  "/ExplorerTest4/",
+  "/ExplorerTest4/index.html",
+  "/ExplorerTest4/manifest.json",
+  "/ExplorerTest4/icons/icon-192.png",
+  "/ExplorerTest4/icons/icon-512.png",
+  "/ExplorerTest4/icons/icon-maskable-192.png",
+  "/ExplorerTest4/icons/icon-maskable-512.png"
 ];
 
-self.addEventListener('install', event => {
+// Install: cache core files
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+// Activate: remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  // Network-first strategy:
+// Fetch: network-first with cache fallback
+self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
